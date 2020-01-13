28c90ec23810fdb32e90c851b090165d8f39725a
eginoy <eginoy.oheso@gmail.com>
2020-01-13 13:59:26 +0900

    バーコード読み取りコンポーネント作成

---
+++ src/components/BarcodeReader.vue
@@ -0,0 +1,95 @@
+<template>
+  <div>
+    <div>
+      scanCode:<span>{{ code }}</span>
+    </div>
+    <button v-on:click="startScan">scan start</button>
+    <div v-show="isScan" id="interactive" class="viewport"></div>
+  </div>
+</template>
+
+<script>
+import Quagga from "../../node_modules/quagga/dist/quagga.js";
+
+// function calc(isbn) {
+//   const arrIsbn = isbn
+//     .toString()
+//     .split("")
+//     .map(function(num) {
+//       parseInt(num);
+//     });
+//   let remainder = 0;
+//   const checkDigit = arrIsbn.pop();
+
+//   arrIsbn.forEach(function(num, index) {
+//     remainder += num * (index % 2 === 0 ? 1 : 3);
+//   });
+//   remainder %= 10;
+//   remainder = remainder === 0 ? 0 : 10 - remainder;
+
+//   return checkDigit === remainder;
+// }
+export default {
+  data() {
+    return {
+      code: null,
+      isScan: false
+    };
+  },
+  mounted() {
+    //dom生成されたあとにQuaggaの初期化する。 (描写位置のエレメントをQuagga初期化時に使用するため)
+  },
+  methods: {
+    startScan: function() {
+      Quagga.init(
+        {
+          inputStream: {
+            name: "Live",
+            type: "LiveStream",
+            target: document.querySelector("#interactive"), //埋め込んだdivのID
+            constraints: {
+              facingMode: "environment"
+            },
+            area: {
+              //必要ならバーコードの読み取り範囲を調整できる（この場合は、上30%/下30%は読み取りしない）
+              top: "30%",
+              right: "0%",
+              left: "0%",
+              bottom: "30%"
+            }
+          },
+          locator: {
+            patchSize: "medium",
+            halfSample: true
+          },
+          numOfWorkers: 2,
+          decoder: {
+            readers: ["ean_reader"] //ISBNは基本的にこれ（他にも種類あり）
+          },
+          locate: true
+        },
+        err => {
+          if (!err) {
+            Quagga.start();
+            // alert("started");
+          }
+        }
+      );
+      this.isScan = true;
+    }
+  },
+  updated() {
+    this.$nextTick(function() {
+      const self = this;
+      Quagga.onDetected(function(success) {
+        const readCode = success.codeResult.code;
+        Quagga.stop();
+        self.isScan = false;
+        return (self.code = readCode);
+      });
+    });
+  }
+};
+</script>
+
+<style scoped></style>
