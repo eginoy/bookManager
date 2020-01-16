3a9f9f3cc97218c6569ecbcbd8ff70c72f5e3068
eginoy <eginoy.oheso@gmail.com>
2020-01-16 20:37:01 +0900

    ・CommonHeader作成 ・vue-router追加

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <CommonHeader></CommonHeader>
     <BookInfo></BookInfo>
     <BarcodeReader></BarcodeReader>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import CommonHeader from "./components/CommonHeader";
 import BarcodeReader from "./components/BarcodeReader";
 import BookInfo from "./components/BookInfo";
 
 export default {
   name: "app",
   components: {
-    HelloWorld,
+    CommonHeader,
     BarcodeReader,
     BookInfo
   }
