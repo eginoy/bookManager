67684a4a3e7349f7b312e1038f6658f469350ce3
eginoy <eginoy.oheso@gmail.com>
2020-01-19 15:56:49 +0900

    書籍情報登録処理追加

--- src/components/BookInfo.vue
+++ src/components/BookInfo.vue
@@ -6,9 +6,15 @@
       </div>
       <div class="p-bookInfo-detail">
         <div class="p-bookInfo-detail-title">{{ bookTitle }}</div>
-        <a class="p-bookInfo-detail-link" v-bind:href="bookLink">Amazonで検索</a>
+        <a class="p-bookInfo-detail-link" v-bind:href="bookLink"
+          >Amazonで検索</a
+        >
       </div>
-      <button class="btn btn-primary" value="登録" />
+      <button
+        class="btn btn-primary"
+        value="登録"
+        v-on:click="registerBookInfo"
+      />
     </div>
     <div v-else>
       <div>{{ bookInfo }}</div>
@@ -18,12 +24,13 @@
 
 <script>
 import $ from "jquery";
+import firebase from "firebase";
 
 export default {
   data() {
     return {
       isbn: "",
-      bookInfo: null,
+      bookInfo: [],
       isSearchResultEmpty: true,
       items: "",
       bookTitle: "",
@@ -58,20 +65,60 @@ export default {
     },
     setBookInfo: function(result) {
       const self = this;
-      self.bookInfo = result;
       self.items = result.items[0].volumeInfo;
       self.bookTitle = self.items.title;
       self.bookImage = self.items.imageLinks.smallThumbnail;
       self.bookIsbnCode10 = self.items.industryIdentifiers[0].identifier;
       self.bookIsbnCode13 = self.items.industryIdentifiers[1].identifier;
       self.bookLink = `https://www.amazon.co.jp/s?k=${self.bookIsbnCode10}&__mk_ja_JP=%E3%82%AB%E3%82%BF%E3%82%AB%E3%83%8A&ref=nb_sb_noss`;
+    },
+    registerBookInfo: function() {
+      const self = this;
+      var db = firebase.database();
+      var ref = db.ref("server/saving-data");
+      var booksRef = ref.child("books");
+
+      booksRef.push({
+        bookTitle: self.bookTitle,
+        bookImage: self.bookImage,
+        bookIsbnCode10: self.bookIsbnCode10,
+        bookIsbnCode13: self.bookIsbnCode13,
+        bookLink: self.bookLink
+      });
+
+      // var usersRef = ref.child("users");
+      // usersRef.set({
+      //   alanisawesome: {
+      //     date_of_birth: "June 23, 1912",
+      //     full_name: "Alan Turing"
+      //   },
+      //   gracehop: {
+      //     date_of_birth: "December 9, 1906",
+      //     full_name: "Grace Hopper"
+      //   }
+      // });
+      //   firebase
+      //     .database()
+      //     .ref("bookInfo")
+      //     .push(
+      //       {
+      //         bookTitle: self.bookTitle,
+      //         bookImage: self.bookImage,
+      //         bookIsbnCode10: self.bookIsbnCode10,
+      //         bookIsbnCode13: self.bookIsbnCode13,
+      //         bookLink: self.bookLink
+      //       },
+      //       function() {
+      //         alert("登録しました");
+      //       }
+      //     );
+      // }
     }
   },
   created: function() {
     // this.getBookInfo();
     this.$eventHub.$on("success-scan", this.getBookInfo);
-  },
-  computed: {}
+  }
 };
 </script>
 
