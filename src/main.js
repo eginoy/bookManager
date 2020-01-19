204c810e35c310289619296bc7a6ec289a850fb7
eginoy <eginoy.oheso@gmail.com>
2020-01-19 15:57:23 +0900

    firebase追加

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import App from "./App.vue";
 
+import firebase from "firebase";
+
 import { BootstrapVue, IconPlugin } from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue";
@@ -23,6 +25,20 @@ Vue.prototype.$eventHub = new Vue();
 
 Vue.use(VueRouter, BootstrapVue, IconPlugin);
 Vue.config.productionTip = false;
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBqTUMBDPsd4Nl9sS7RrwPh3QaHhFyqjq4",
+  authDomain: "bookmanager-5ba4c.firebaseapp.com",
+  databaseURL: "https://bookmanager-5ba4c.firebaseio.com",
+  projectId: "bookmanager-5ba4c",
+  storageBucket: "bookmanager-5ba4c.appspot.com",
+  messagingSenderId: "991241456327",
+  appId: "1:991241456327:web:933a53fa86a3fa5e7f0ce2",
+  measurementId: "G-PNHNDBKK49"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 new Vue({
   render: h => h(App)
