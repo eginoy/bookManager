d76e0d1d5eb9054b645a845593a319a0dc5f3399
eginoy <eginoy.oheso@gmail.com>
2020-01-26 21:46:28 +0900

    vue create でマージ後調整と本番用ビルド作成

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,24 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import BooksList from '../components/BooksList'
+import BookRegister from '../components/BookRegister'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
-
-const router = new VueRouter({
-  routes
+export default new VueRouter({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      component: BooksList
+    },
+    {
+      path: '/books',
+      component: BooksList
+    },
+    {
+      path: '/bookRegistration',
+      component: BookRegister
+    }
+  ]
 })
-
-export default router
